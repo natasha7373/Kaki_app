708a9ed7f3345d1723d34fb88651c6e3728a4c9b
Ashtonlau99 <iamashtonlau@gmail.com>
2022-08-12 16:54:58 +0800

    first commit

--- app/src/main/java/com/example/kakihomeui/sign_up.java
+++ app/src/main/java/com/example/kakihomeui/sign_up.java
@@ -1,14 +1,49 @@
 package com.example.kakihomeui;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 
 public class sign_up extends AppCompatActivity {
+    EditText full_name,phone_number,password,password_conf;
+    Button submit;
+    private ProgressDialog loadingBar;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
+    DatabaseReference UsersReference;
+    String currentUserID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth= FirebaseAuth.getInstance();
+        submit=(Button)findViewById(R.id.sign_up);
+
+
     }
 }
