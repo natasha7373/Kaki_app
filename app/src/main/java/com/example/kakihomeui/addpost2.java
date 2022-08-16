f72834ecf4e1ffa072af4b3e4207143df16d9567
hy329j99 <junghyungyun012@gmail.com>
2022-08-16 23:10:30 +0800

    addpost1 to addpost2 button

--- app/src/main/java/com/example/kakihomeui/addpost2.java
+++ app/src/main/java/com/example/kakihomeui/addpost2.java
@@ -2,7 +2,10 @@ package com.example.kakihomeui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class addpost2 extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class addpost2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addpost2);
+
+        TextView Back;
+        Back = findViewById(R.id.back);
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(addpost2.this,addpost1.class);
+                startActivity(i);
+            }
+        });
     }
 }
